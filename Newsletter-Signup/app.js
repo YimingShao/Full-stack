464c86bac76d8d2e668fd9086b3bc8958bb2be78
YimingShao <shaoyiminglobo@gmail.com>
2020-05-05 14:15:23 -0400

    Commit

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -3,45 +3,71 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
-    extended: true
+  extended: true
 }));
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/signup.html");
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function (req, res) {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+app.post("/", function(req, res) {
 
-    var options = {
-        url: "https://us18.api.mailchimp.com/3.0/lists/40e9ba3a38",
-        method: "POST",
-        headers: {
-            "Authorization": "Yiming e0707ab7db0c2498f43656382fe9ab9f-us4"
-        }
-    };
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-    request(options, function (error, response, body) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(response.statusCode);
-        }
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  const url = "https://us4.api.mailchimp.com/3.0/lists/40e9ba3a38";
+
+  const options = {
+    method: "POST",
+    auth: "Yiming:20f46cc0731c93714c1b53fd6e600f05-us4"
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode == 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
     })
+  })
+  request.write(jsonData);
+  request.end();
+
 });
 
-
-app.listen(3000, function () {
-    console.log("Server is running");
+app.post("/failure", function(req, res) {
+  res.redirect("/");
 });
 
+app.listen(3000, function() {
+  console.log("Server is running on port 3000");
+});
 
+//API Key
+//20f46cc0731c93714c1b53fd6e600f05-us4
+
+//List // ID
 //40e9ba3a38
-//e0707ab7db0c2498f43656382fe9ab9f-us4
